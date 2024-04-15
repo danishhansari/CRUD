8667cacecfb375bd4e989f8ccbde1dc4815c19f5
Danish <dan71ish@gmail.com>
2024-04-15 22:39:30 +0530

    set cors

--- server/app.js
+++ server/app.js
@@ -1,15 +1,8 @@
 import express from "express";
-import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
@@ -18,7 +11,7 @@ app.use(cookieParser());
 // routes import
 import userRouter from "./routes/user.router.js";
 
-app.use("/api/v1/users", userRouter);
+app.use("/api/users", userRouter);
 
 app.get("/", (req, res) => {
   return res.status(200).json({ message: "App is running fine" });
