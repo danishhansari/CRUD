7364506e1edb53ce6bcbf313191bd3fe7077d938
Danish <dan71ish@gmail.com>
2024-04-16 18:57:23 +0530

    added the auto profile generate for new user

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,6 +2,35 @@ import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
+const profile_imgs_name_list = [
+  "Garfield",
+  "Tinkerbell",
+  "Annie",
+  "Loki",
+  "Cleo",
+  "Angel",
+  "Bob",
+  "Mia",
+  "Coco",
+  "Gracie",
+  "Bear",
+  "Bella",
+  "Abby",
+  "Harley",
+  "Cali",
+  "Leo",
+  "Luna",
+  "Jack",
+  "Felix",
+  "Kiki",
+];
+
+const profile_imgs_collections_list = [
+  "notionists-neutral",
+  "adventurer-neutral",
+  "fun-emoji",
+];
+
 const userSchema = new Schema(
   {
     username: {
@@ -26,8 +55,18 @@ const userSchema = new Schema(
       index: true,
     },
     avatar: {
-      type: String, // cloudinary url
-      // required: true, // will enable this later on
+      type: String,
+      default: () => {
+        return `https://api.dicebear.com/6.x/${
+          profile_imgs_collections_list[
+            Math.floor(Math.random() * profile_imgs_collections_list.length)
+          ]
+        }/svg?seed=${
+          profile_imgs_name_list[
+            Math.floor(Math.random() * profile_imgs_name_list.length)
+          ]
+        }`;
+      },
     },
     coverImage: {
       type: String, // cloudinary url
